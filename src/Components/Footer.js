2ecec44dfa4fe0cc0bb9a72d7bc33e46faf4543e
mackinator3 <mackinator3@gmail.com>
2024-02-07 23:22:44 -0500

    Updates

--- src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Container from "react-bootstrap/Container";
-
-export default function Footer() {
-    return (
-        <Container className="footer" sticky="bottom">
-            MackNation Industry Test
-        </Container>
-    )
-}
