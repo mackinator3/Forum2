3f135fa6d837f1346d67d39cf580ef96772d1fbd
mackinator3 <mackinator3@gmail.com>
2024-05-06 15:46:22 -0400

    Added some css to fix the footer to the bottom for multiple pages. Hid the nav for video games, it ain't ready yet.

--- src/Components/newsData.js
+++ src/Components/newsData.js
@@ -18,6 +18,11 @@ const news = [
         id: 12,
         date: "2/21/2024",
         text: "Worked a bit on the css on the homepage. I've been doing a lot of slow progress over time, hoping it turns into something good. These dates aren't accurate, as I was doing a lot of tests. I will try to keep them more accurate now."
+    },
+    {
+        id: 13,
+        date: "5/6/2024",
+        text: "Added some css to fix the footer to the bottom for multiple pages. Hid the nav for video games, it ain't ready yet."
     }
 
 ]
