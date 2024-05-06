ec51866216071e46638bdc9f440f7762aa6f70a0
mackinator3 <mackinator3@gmail.com>
2024-05-06 15:51:00 -0400

    Fixed news item bug.

--- src/Components/newsData.js
+++ src/Components/newsData.js
@@ -5,22 +5,22 @@ const news = [
         text: "This is the first big update. This tracker will eventually need outside sourcing."
     },
     {
-        id: 10,
+        id: 2,
         date: "1/3/2024",
         text: "Anime still needs CSS, same for Star Wars."
     },
     {
-        id: 11,
+        id: 3,
         date: "2/8/2024",
         text: "Github pages works. I am working on CSS now."
     },
     {
-        id: 12,
+        id: 4,
         date: "2/21/2024",
         text: "Worked a bit on the css on the homepage. I've been doing a lot of slow progress over time, hoping it turns into something good. These dates aren't accurate, as I was doing a lot of tests. I will try to keep them more accurate now."
     },
     {
-        id: 13,
+        id: 5,
         date: "5/6/2024",
         text: "Added some css to fix the footer to the bottom for multiple pages. Hid the nav for video games, it ain't ready yet."
     }
